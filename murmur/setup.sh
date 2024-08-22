07eb3f43c51efd04a703be9753457381fff3da78
cassius23 <emilekm@gmail.com>
2024-08-22 17:00:55 +0200

    Murmur init

---
+++ murmur/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -f ".murmursetup" ]]; then
+    echo "Skipping Setup - already configured"
+else
+    cd mumo/tools
+
+    python prbf2setup.py -s prmurmurpassword -i "../../PRMurmur.ice"
+
+    python prbf2man.py -c $CHANNEL_ID -n "$SERVER_NAME" -f $GAMESERVER_IP -b 1 -o "../modules-enabled/prbf2.ini" -s prmurmurpassword -i "../../PRMurmur.ice"
+
+    if [[ -v MURMUR_SU_PASS ]]; then
+        ./prmurmurd.x64 -supw $MURMUR_SU_PASS
+    fi
+
+    touch .murmursetup
+fi
