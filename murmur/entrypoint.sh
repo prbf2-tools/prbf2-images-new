7fef16d9d9f3a21d2101c853ccb513ed0dd0e62c
cassius23 <emilekm@gmail.com>
2025-12-11 23:02:18 +0100

    Allow for changing mumo port

--- murmur/entrypoint.sh
+++ murmur/entrypoint.sh
@@ -8,6 +8,12 @@ if [[ -v MURMUR_PORT ]]; then
     sed -i "s/^port=.*/port=${MURMUR_PORT}/" ./prmurmur.ini
 fi
 
+if [[ -v MUMO_PORT ]]; then
+    echo "Setting port to ${MUMO_PORT}"
+    sed -i "s/^ice=\"tcp -h 127.0.0.1 -p .*\"/ice=\"tcp -h 127.0.0.1 -p ${MUMO_PORT}\"/" ./prmurmur.ini
+    sed -i "s/^port = .*/port = ${MUMO_PORT}/" ./mumo/mumo.ini
+fi
+
 # start the server
 ./prmurmurd.x64 -fg &
 
